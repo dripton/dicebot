3553b8c657a147fbd4b86c8fe81bd30514164c5b
David Ripton <dripton@ripton.net>
2018-07-15 17:44:05 -0400

    Dice rolling and expression evaluation
    Used the shunting yard algorithm to turn the infix expression into
    postfix, then a basic RPN evaluator.
    
    Currently all functions take exactly 2 arguments.

--- dicebot.py
+++ dicebot.py
@@ -2,6 +2,7 @@
 
 import random
 import sys
+from typing import List, Tuple
 
 import discord
 from discord.ext import commands
@@ -18,24 +19,166 @@ async def on_ready():
     print("------")
 
 
-def roll_inner(cmd : str):
-    dice, sides = map(int, cmd.split("d"))
+def d(dice: int, sides: int) -> Tuple[str, List[int], int]:
     rolls = [random.randint(1, sides) for r in range(dice)]
+    total = sum(rolls)
     if rolls:
-        result = " + ".join(map(str, rolls)) + " = %d" % sum(rolls)
+        result = " + ".join(map(str, rolls)) + " = %d" % total
     else:
         result = ""
+    return (result, rolls, total)
+
+
+def gen_tokens(cmd: str):
+    """Yield tokens."""
+    in_whitespace = False
+    in_number = False
+    in_alpha = False
+    partial = ""
+    for ii, ch in enumerate(cmd):
+        if ch.isspace():
+            if in_whitespace:
+                partial += ch
+            else:
+                if partial:
+                    yield partial
+                in_whitespace = True
+                in_number = False
+                in_alpha = False
+                partial = ch
+        elif ch.isdigit() or ch == ".":
+            if in_number:
+                partial += ch
+            else:
+                if partial:
+                    yield partial
+                in_number = True
+                in_whitespace = False
+                in_alpha = False
+                partial = ch
+        elif ch.isalpha():
+            if in_alpha:
+                partial += ch
+            else:
+                if partial:
+                    yield partial
+                in_alpha = True
+                in_whitespace = False
+                in_number = False
+                partial = ch
+        else:
+            if partial:
+                yield partial
+                partial = ""
+            in_alpha = False
+            in_whitespace = False
+            in_number = False
+            yield ch
+    if partial:
+        yield partial
+
+
+operators = {
+    "*": 3,
+    "/": 3,
+    "+": 2,
+    "-": 2,
+}
+
+
+def to_postfix(tokens: List[str]) -> List[str]:
+    """ https://en.wikipedia.org/wiki/Shunting-yard_algorithm """
+    output_queue = []
+    operator_stack = []
+    for token in tokens:
+        if token[0].isspace():
+            pass
+        elif token[0].isdigit() or token[0] == ".":
+            output_queue.append(token)
+        elif token[0].isalpha():
+            operator_stack.append(token)
+        elif token[0] in operators:
+            precedence = operators[token[0]]
+            while operator_stack and (
+                    operator_stack[-1][0].isalpha() or
+                    operators.get(operator_stack[-1], 0) >= precedence):
+                operator = operator_stack.pop()
+                output_queue.append(operator)
+            operator_stack.append(token)
+        elif token == "(":
+            operator_stack.append(token)
+        elif token == ")":
+            while operator_stack and operator_stack[-1] != "(":
+                operator = operator_stack.pop()
+                output_queue.append(operator)
+            if operator_stack and operator_stack[-1] == "(":
+                operator_stack.pop()
+                # else mismatched parentheses
+    while operator_stack:
+        operator = operator_stack.pop()
+        output_queue.append(operator)
+    return output_queue
+
+
+def to_number(arg):
+    if isinstance(arg, str):
+        if "." in arg:
+            return float(arg)
+        else:
+            return int(arg)
+    return arg
+
+
+def rpn_evaluate(output_queue: List[str]) -> float:
+    eval_stack = []
+    for token in output_queue:
+        if token[0].isdigit() or token[0] == ".":
+            eval_stack.append(token)
+        elif token[0].isalpha():
+            # TODO 1-arg vs. 2-arg vs. "d" function  d vs. d6 vs. 3d6 or 4d
+            if token[0].lower() == "d":
+                arg2 = eval_stack.pop()
+                sides = int(arg2)
+                arg1 = eval_stack.pop()
+                dice = int(arg1)
+                result, rolls, total = d(dice, sides)
+                # TODO show result
+                eval_stack.append(total)
+            else:
+                raise Exception("bogus function")
+        elif token in operators:
+            arg2 = to_number(eval_stack.pop())
+            arg1 = to_number(eval_stack.pop())
+            if token == "+":
+                total = arg1 + arg2
+            elif token == "-":
+                total = arg1 - arg2
+            elif token == "*":
+                total = arg1 * arg2
+            elif token == "/":
+                total = arg1 / arg2
+            # TODO bogus operator
+            eval_stack.append(total)
+    return eval_stack[0]
+
+
+def roll_inner(cmd: str) -> float:
+    tokens = list(gen_tokens(cmd))
+    output_queue = to_postfix(tokens)
+    result = rpn_evaluate(output_queue)
     return result
 
 
 @bot.command()
-async def roll(cmd : str):
+async def roll(cmd: str):
     """Rolls dice in roll20 format."""
+    print("roll(%s)" % cmd)
     try:
         result = roll_inner(cmd)
     except Exception:
         await bot.say("invalid format")
         return
+    print("result", result)
     await bot.say(result)
 
 
--- test_dicebot.py
+++ test_dicebot.py
@@ -1,15 +1,92 @@
 import pytest
 
-from dicebot import roll_inner
+import dicebot
+
+
+def test_to_postfix():
+    assert dicebot.to_postfix([]) == []
+    assert dicebot.to_postfix(["1"]) == ["1"]
+    assert dicebot.to_postfix(["1", "2"]) == ["1", "2"]
+    assert dicebot.to_postfix(["1", "+", "2"]) == ["1", "2", "+"]
+
 
 def test_empty():
     with pytest.raises(TypeError):
-        roll_inner()
+        dicebot.roll_inner()
+
 
 def test_nds():
-    result = roll_inner("3d1")
-    assert result == "1 + 1 + 1 = 3"
+    result = dicebot.roll_inner("3d1")
+    assert result == 3
+    result = dicebot.roll_inner("3D1")
+    assert result == 3
+
+
+def test_bogus_function():
+    with pytest.raises(Exception):
+        dicebot.roll_inner("3q1")
+
 
 def test_0ds():
-    result = roll_inner("0d1")
-    assert result == ""
+    result = dicebot.roll_inner("0d1")
+    assert result == 0
+
+
+def test_gen_tokens():
+    assert list(dicebot.gen_tokens("")) == []
+    assert list(dicebot.gen_tokens("1")) == ["1"]
+    assert list(dicebot.gen_tokens("d")) == ["d"]
+    assert list(dicebot.gen_tokens("abc")) == ["abc"]
+    assert list(dicebot.gen_tokens("7.5")) == ["7.5"]
+    assert list(dicebot.gen_tokens("3d6")) == ["3", "d", "6"]
+    assert list(dicebot.gen_tokens("3d6 + 5")) == ["3", "d", "6", " ", "+", " ", "5"]
+    assert list(dicebot.gen_tokens("18d6*3+5")) == ["18", "d", "6", "*", "3", "+", "5"]
+
+
+def test_addition():
+    result = dicebot.roll_inner("1 + 1")
+    assert result == 2
+    result = dicebot.roll_inner("1 + 1 + 2")
+    assert result == 4
+
+
+def test_subtraction():
+    result = dicebot.roll_inner("1 - 1")
+    assert result == 0
+
+
+def test_multiplication():
+    result = dicebot.roll_inner("2 * 3")
+    assert result == 6
+
+
+def test_division():
+    result = dicebot.roll_inner("8 / 4")
+    assert result == 2
+    result = dicebot.roll_inner("10 / 4")
+    assert result == 2.5
+
+
+def test_adding_dice():
+    result = dicebot.roll_inner("1d1 + 1d1")
+    assert result == 2
+
+
+def test_parenthesis():
+    result = dicebot.roll_inner("(1 + 2) * 3")
+    assert result == 9
+
+
+def test_dice_plus_operators():
+    result = dicebot.roll_inner("1d1 + 100")
+    assert result == 101
+    result = dicebot.roll_inner("1d1 - 100")
+    assert result == -99
+    result = dicebot.roll_inner("1d1 * 100")
+    assert result == 100
+    result = dicebot.roll_inner("1d1 / 100")
+    assert result == 0.01
+    result = dicebot.roll_inner("100d1 / 10")
+    assert result == 10
+    result = dicebot.roll_inner("100d1 / 10")
+    assert result == 10
